8239302affe77476eff73df2e60c3a2c25c20ef3
王命风 <wangmingfeng@qutoutiao.net>
2020-06-07 23:36:30 +0800

    :construction: deploy

--- next.config.js
+++ next.config.js
@@ -3,5 +3,16 @@ const withPWA = require("next-pwa");
 module.exports = withPWA({
   pwa: {
     dest: "public",
-  }
+  },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.node = {
+      fs: 'empty',
+      net: 'empty',
+      tls: 'empty'
+    }
+    return config
+  },
+  webpackDevMiddleware: config => {
+    return config
+  },
 });
